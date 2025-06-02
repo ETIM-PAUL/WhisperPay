1999b8abeb22639fadb281b99f0a992fba1da4d1
undefined <etimpaul22@gmail.com>
2025-06-02 01:34:13 +0100

    testing reverse fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,4 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-const path = require('path');
-import nodePolyfills from 'vite-plugin-node-stdlib-browser'
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-
   return {
     resolve: {
       alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
@@ -14,7 +8,12 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [reactRefresh(), nodePolyfills()],
     build: {
-      target: ['es2020', 'chrome90', 'firefox90', 'safari15'], // Updated targets to support BigInt
+      target: ['es2020', 'chrome90', 'firefox90', 'safari15'],
+      minify: 'terser', // Change from esbuild to terser
+      terserOptions: {
+        keep_classnames: true,
+        keep_fnames: true
+      }
     },
   };
 });
